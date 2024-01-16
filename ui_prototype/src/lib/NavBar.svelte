4f561fc62719fdf3153f5f07cd4cffd90a08a120
Soushi888 <sacha.pignot@protonmail.com>
2024-01-16 07:17:05 -0500

    favicon + minor css change

--- ui_prototype/src/lib/NavBar.svelte
+++ ui_prototype/src/lib/NavBar.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import bars from '$lib/assets/svg/bars.svg';
+	import Bars from '$lib/assets/svg/bars.svelte';
 </script>
 
-<nav class="bg-primary-500 flex p-2 justify-between items-center h-32 absolute w-full">
+<nav class="bg-primary-500 flex p-4 justify-between items-center h-32 absolute w-full">
 	<a href="/"><img src="/hAppeningsLogoWsun2.webp" alt="" class="h-28" /></a>
 
 	<h1 class="h1 text-white text-center">Requests and Offerts <br />hApp</h1>
 
-	<button type="button" class="btn variant-filled-secondary">
-		<img src={bars} alt="hamburger menu" />
+	<button type="button" class="btn variant-filled-secondary p-4 group">
+		<Bars svgClass={`fill-[#4720b7] group-hover:fill-white`} />
 	</button>
 </nav>
--- ui_prototype/src/routes/+page.svelte
+++ ui_prototype/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-	import { TableOfContents, tocCrawler } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
 </script>
 
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<p class="mb-10 h3">It looks like you don't have a profile yet !</p>
-<div class="btn variant-filled-primary"><a href="/profiles/create">Create profile</a></div>
+<section class="flex flex-col items-center">
+	<p class="mb-10 h3">It looks like you don't have a profile yet !</p>
+	<button class="btn variant-filled-primary w-fit" on:click={() => goto('/profiles/create')}>
+		Create profil
+	</button>
+</section>
