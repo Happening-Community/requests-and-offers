7743d4770471c45be4cda397e6cdbcb8dbb85d26
Soushi888 <sacha.pignot@protonmail.com>
2024-04-06 03:41:43 -0400

    responsive hamburger menu

---
+++ ui/src/lib/MenuLinks.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { myProfile } from '@stores/profiles.store';
+</script>
+
+<div class="flex flex-col gap-3">
+  {#if $myProfile}
+    <a href="/profile" class="btn variant-filled-secondary">My profile</a>
+  {:else}
+    <a href="/profile/create" class="btn variant-filled-secondary">Create profile</a>
+  {/if}
+  <a href="/profiles" class="btn variant-filled-secondary">All profiles</a>
+</div>
+<div class="flex flex-col gap-3">
+  <a href="/projects" class="btn variant-filled-secondary">Projects</a>
+  <a href="/organizations" class="btn variant-filled-secondary">Organizations</a>
+</div>
+<div class="flex flex-col gap-3">
+  <a href="/requests" class="btn variant-filled-secondary">Requests</a>
+  <a href="/offers" class="btn variant-filled-secondary">Offers</a>
+</div>
--- ui/src/lib/NavBar.svelte
+++ ui/src/lib/NavBar.svelte
@@ -1,16 +1,43 @@
 <script lang="ts">
-  import { myProfile } from '@stores/profiles.store';
+  import Bars from './svg/bars.svelte';
+  import { getDrawerStore, type DrawerSettings, Drawer } from '@skeletonlabs/skeleton';
+  import MenuLinks from './MenuLinks.svelte';
+
+  const drawerStore = getDrawerStore();
+  const drawerSettings: DrawerSettings = {
+    id: 'menu-drawer',
+    width: 'w-1/3',
+    height: 'h-full',
+    bgDrawer: 'bg-primary-500'
+  };
+
+  function openDrawer() {
+    drawerStore.open(drawerSettings);
+  }
 </script>
 
+<Drawer>
+  {#if $drawerStore.id === 'menu-drawer'}
+    <div class="flex h-10 flex-col gap-5 p-5 text-2xl">
+      <MenuLinks />
+    </div>
+  {/if}
+</Drawer>
+
 <nav class="bg-primary-500 absolute flex h-32 w-full items-center justify-between gap-4 p-4">
-  <a href="/" class="w-1/3"><img src="/hAppeningsLogoWsun2.webp" alt="" class="w-28" /></a>
+  <a href="/" class="w-1/3">
+    <img src="/hAppeningsLogoWsun2.webp" alt="" class="w-28" />
+  </a>
 
   <a class="h2 text-center text-white" href="/">Requests and Offers hApp</a>
 
-  <div class="flex w-1/3 flex-col justify-end gap-4 sm:flex-row">
-    {#if $myProfile}
-      <a href="/profile" class="btn variant-filled-secondary">My profile</a>
-    {/if}
-    <a href="/profiles" class="btn variant-filled-secondary">All profiles</a>
+  <div class="flex w-1/3 justify-end gap-4">
+    <button class="btn variant-filled-secondary lg:hidden" on:click={openDrawer}>
+      <Bars svgClass="fill-white h-6 w-6" />
+    </button>
+  </div>
+
+  <div class=" hidden w-1/3 flex-col justify-end gap-4 sm:flex-row lg:flex">
+    <MenuLinks />
   </div>
 </nav>
