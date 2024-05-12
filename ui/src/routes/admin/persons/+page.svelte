b64c3e7bda6aca31bc48af3909d35da6f6532ed2
Soushi888 <sacha.pignot@protonmail.com>
2024-05-12 07:56:27 -0400

    pending and accepted profiles ui

--- ui/src/routes/admin/persons/+page.svelte
+++ ui/src/routes/admin/persons/+page.svelte
@@ -16,17 +16,14 @@
 
   $: isLoading = true;
 
+  const pendingProfiles = $profiles.filter((profile) => profile.status === 'pending');
+  const acceptedProfiles = $profiles.filter((profile) => profile.status === 'accepted');
+  const rejectedProfiles = $profiles.filter((profile) => profile.status === 'rejected');
+
   const conicStops: ConicStop[] = [
     { color: 'transparent', start: 0, end: 0 },
     { color: 'rgb(var(--color-secondary-500))', start: 75, end: 50 }
   ];
-
-  onMount(async () => {
-    await getAllProfiles();
-    isLoading = false;
-    profilesHashes = (await getAllProfilesLinks()).map((profile) => profile.target);
-    console.log('profilesHashes :', profilesHashes);
-  });
 
   const modalStore = getModalStore();
   const modalComponent: ModalComponent = { ref: ProfileDetailsModal };
@@ -40,53 +37,104 @@
       }
     };
   };
+
+  onMount(async () => {
+    await getAllProfiles();
+    isLoading = false;
+    profilesHashes = (await getAllProfilesLinks()).map((profile) => profile.target);
+    console.log('profilesHashes :', profilesHashes);
+  });
 </script>
 
 <section class="flex flex-col gap-10 text-white">
   <h1 class="h1 text-center">Persons management</h1>
-  <div class="flex flex-col gap-4">
-    <h2 class="h3">Pending persons</h2>
-    {#if $profiles && $profiles.length > 0}
-      <table class="table-hover table drop-shadow-lg">
-        <thead>
-          <tr>
-            <th>Avatar</th>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each $profiles as profile, i}
+  {#if isLoading && $profiles.length === 0}
+    <ConicGradient stops={conicStops} spin>Loading</ConicGradient>
+  {/if}
+  <div class="flex flex-col gap-4 lg:flex-row lg:gap-0 lg:divide-x-2">
+    <div class="flex flex-col gap-4 lg:pr-4">
+      <h2 class="h3">Pending persons</h2>
+      {#if pendingProfiles && pendingProfiles.length > 0}
+        <table class="table-hover table drop-shadow-lg">
+          <thead>
             <tr>
-              <td>
-                <Avatar
-                  src={profile.picture
-                    ? URL.createObjectURL(new Blob([new Uint8Array(profile.picture)]))
-                    : '/default_avatar.webp'}
-                />
-              </td>
-              <td>{profile.name}</td>
-              <td>
-                {profile.user_type.charAt(0).toUpperCase() + profile.user_type.slice(1)}
-              </td>
-              <td>
-                <button
-                  class="btn variant-filled-secondary"
-                  on:click={() => modalStore.trigger(modal(i, profilesHashes[i]))}
-                >
-                  View
-                </button>
-              </td>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Actions</th>
             </tr>
-          {/each}
-        </tbody>
-      </table>
-    {:else}
-      <p>No pending profiles</p>
-    {/if}
-    {#if isLoading && $profiles.length === 0}
-      <ConicGradient stops={conicStops} spin>Loading</ConicGradient>
-    {/if}
+          </thead>
+          <tbody>
+            {#each pendingProfiles as profile, i}
+              <tr>
+                <td>
+                  <Avatar
+                    src={profile.picture
+                      ? URL.createObjectURL(new Blob([new Uint8Array(profile.picture)]))
+                      : '/default_avatar.webp'}
+                  />
+                </td>
+                <td>{profile.name}</td>
+                <td>
+                  {profile.user_type.charAt(0).toUpperCase() + profile.user_type.slice(1)}
+                </td>
+                <td>
+                  <button
+                    class="btn variant-filled-secondary"
+                    on:click={() => modalStore.trigger(modal(i, profilesHashes[i]))}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      {:else}
+        <p>No pending profiles</p>
+      {/if}
+    </div>
+    <div class="flex flex-col gap-4 lg:pl-4">
+      <h2 class="h3">Accepted persons</h2>
+      {#if acceptedProfiles && acceptedProfiles.length > 0}
+        <table class="table-hover table drop-shadow-lg">
+          <thead>
+            <tr>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each acceptedProfiles as profile, i}
+              <tr>
+                <td>
+                  <Avatar
+                    src={profile.picture
+                      ? URL.createObjectURL(new Blob([new Uint8Array(profile.picture)]))
+                      : '/default_avatar.webp'}
+                  />
+                </td>
+                <td>{profile.name}</td>
+                <td>
+                  {profile.user_type.charAt(0).toUpperCase() + profile.user_type.slice(1)}
+                </td>
+                <td>
+                  <button
+                    class="btn variant-filled-secondary"
+                    on:click={() => modalStore.trigger(modal(i, profilesHashes[i]))}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      {:else}
+        <p>No accepted profiles</p>
+      {/if}
+    </div>
   </div>
 </section>
