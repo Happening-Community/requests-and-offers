a08ece2c8f141df30cd6d396b942fe8518212961
Soushi888 <sacha.pignot@protonmail.com>
2024-06-23 19:53:41 -0400

    external call for get_agent_profile_hash in administration zome

---
+++ dnas/requests_and_offers/zomes/coordinator/administration/src/external_calls.rs
@@ -0,0 +1,20 @@
+use crate::wasm_error;
+use hdk::prelude::*;
+
+pub fn get_agent_profile_hash(agent_pubkey: AgentPubKey) -> ExternResult<Option<ActionHash>> {
+    let zome_call_response = call(
+        CallTargetCell::Local,
+        ZomeName("profiles".into()),
+        FunctionName("get_agent_profile_hash".into()),
+        None,
+        agent_pubkey.clone(),
+    )?;
+
+    if let ZomeCallResponse::Ok(response) = zome_call_response {
+        Ok(response.decode().unwrap())
+    } else {
+        Err(wasm_error(
+            "Error while calling the get_agent_profile_hash function of the profiles zome",
+        ))
+    }
+}
