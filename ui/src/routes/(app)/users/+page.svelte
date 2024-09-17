8b050dd5bf33046752892d5f84c7233af60befe0
soushi888 <sacha.pignot@protonmail.com>
2024-09-17 00:02:04 -0400

    🐞 fix(ui|users page): fix the AcceptedEntities vrariables and functions names

--- ui/src/routes/(app)/users/+page.svelte
+++ ui/src/routes/(app)/users/+page.svelte
@@ -5,7 +5,7 @@
   import usersStore from '@stores/users.svelte';
   import { onMount } from 'svelte';
 
-  const { myProfile, AcceptedEntityyyy } = $derived(usersStore);
+  const { myProfile, AcceptedEntities } = $derived(usersStore);
 
   const conicStops: ConicStop[] = [
     { color: 'transparent', start: 0, end: 0 },
@@ -15,7 +15,7 @@
   let isLoading = $state(true);
 
   onMount(async () => {
-    await usersStore.getAcceptedEntity();
+    await usersStore.getAcceptedUsers();
     isLoading = false;
   });
 </script>
@@ -27,12 +27,12 @@
       <NavButton href="/user/create">Create Profile</NavButton>
     {/if}
   </div>
-  {#if AcceptedEntity.length}
-    <UsersTable users={AcceptedEntity} />
+  {#if AcceptedEntities.length}
+    <UsersTable users={AcceptedEntities} />
   {:else}
     <p class="h3 text-error-500">No users found.</p>
   {/if}
-  {#if isLoading && AcceptedEntity.length === 0}
+  {#if isLoading && AcceptedEntities.length === 0}
     <ConicGradient stops={conicStops} spin>Loading</ConicGradient>
   {/if}
 </section>
